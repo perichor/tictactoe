407eb025ee117f24ad36d712020097bb9c3e24c9
Jeremy Sande <sandejeremy@gmail.com>
2017-03-13 11:58:17 -0700

    fix second test

--- test/test.js
+++ test/test.js
@@ -8,10 +8,22 @@ describe('Tic Tac Toe', function() {
 		game.makeMove('0 0');
 
 		expect(game.board[0][0]).to.equal('X');
+
+		game.endGame();
 	});
 
 	it('should determine a winner', function() {
-
+		var game = new Game();
+		var board = [
+			['X',' ',' '],
+			[' ',' ',' '],
+			[' ',' ',' ']
+		]
+		game.makeMove('0 2');
+		game.makeMove('1 1');
+		game.makeMove('0 1');
+		console.log(game.board)
+		expect(game.winner).to.equal('X');
 	});
 
 });
